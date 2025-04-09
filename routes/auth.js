67518a2afc42e02c41a61a3364f1e440be2b8b57
MinThantMaung <minthant180@gmail.com>
2025-04-09 13:20:39 +0900

    feat:install libraries

---
+++ routes/auth.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const User = require('../models/User');
+const { generateToken,generateRefreshToken } = require('../utils/jwt');
+const loginLimiter = require('../middleware/loginLimiter');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).send({message: 'User already exists'});
+    const user = new User({email,password});
+    await user.save();
+    res.status(201).json({message: 'User registered successfully'});
+});
+
+router.post('/login',loginLimiter, async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const payload = { userId: user._id, email: user.email, role: user.role };
+    const accessToken = generateToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+
+    // Send refresh token as HttpOnly cookie
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        //secure: process.env.NODE_ENV === 'production',
+        secure: false,
+        sameSite: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    // Send access token in JSON
+    res.json({
+        accessToken,
+        user: {
+            id: user._id,
+            email: user.email,
+        },
+    });
+});
+
+// REFRESH
+router.post('/refresh', (req, res) => {
+    const token = req.cookies.refreshToken;
+
+    if (!token) return res.status(401).json({ message: 'No refresh token provided' });
+
+    try {
+        const payload = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+        const newAccessToken = generateToken({ userId: payload.userId, email: payload.email });
+
+        res.json({ accessToken: newAccessToken });
+    } catch (err) {
+        return res.status(403).json({ message: 'Invalid or expired refresh token' });
+    }
+});
+
+// LOGOUT
+router.post('/logout', (req, res) => {
+    res.clearCookie('refreshToken');
+    res.status(200).json({ message: 'Logged out successfully' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use('/v1/auth', authRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
