14a368d2e72867fafe03efc55e35212624da453d
MinThantMaung <minthant180@gmail.com>
2025-04-09 16:29:01 +0900

    fix:handle dev env

--- server.js
+++ server.js
@@ -18,4 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
